284a28d4f70cea7aee9952a83145d97f52c1f29e
Suyash Sumaroo <suyash@codevigor.com>
2025-03-20 13:48:54 -0400

    added new attributes for home information

--- api/controllers/home.js
+++ api/controllers/home.js
@@ -14,7 +14,11 @@ module.exports = {
         const data = {
             title: "Welcome to the Home Page",
             message: "This is the home page of our application.",
-            additional_message: "This is the official application that has been migrated"
+            additional_message: "This is the official application that has been migrated",
+            additional_info: {
+                message: "This is the official application that has been migrated",
+                record_count: 10
+            }
         };
 
         return data;
